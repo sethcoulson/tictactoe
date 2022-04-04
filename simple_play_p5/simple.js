678f0cb34214a670e1c0ed4e633eeee9cd441ceb
sethcoulson <seth_coulson@me.com>
2022-04-04 19:53:37 -0400

    Winning lines

--- simple_play_p5/simple.js
+++ simple_play_p5/simple.js
@@ -67,17 +67,29 @@ function counts(){
 
 function terminal_state( b ){
     c=counts();
-    if((b[0] != "") && ((b[0]==b[1] && b[1]==b[2]) || (b[0]==b[3] && b[3]==b[6]) || (b[0]==b[4] && b[4]==b[8]))) {        
-      rv = {"terminal": true, "type": "win", "player": b[0] }
+    if(b[0] != "" && b[0]==b[1] && b[1]==b[2]) {        
+      rv = {"terminal": true, "type": "win", "player": b[0], "direction": "r1" }
+    }  
+    else if(b[0] != "" && b[0]==b[3] && b[3]==b[6]) {        
+        rv = {"terminal": true, "type": "win", "player": b[0], "direction": "c1" }
     }      
-    else if((b[4]!="") && ((b[3]==b[4] && b[4]==b[5]) || (b[1]==b[4] && b[4]==b[7]) || (b[2]==b[4] && b[4]==b[6]))){
-        rv = {"terminal": true, "type": "win", "player": b[4] }
+    else if(b[0] != "" && b[0]==b[4] && b[4]==b[8]) {        
+    rv = {"terminal": true, "type": "win", "player": b[0], "direction": "d2" }
+    }      
+    else if(b[4]!="" && b[3]==b[4] && b[4]==b[5]){
+        rv = {"terminal": true, "type": "win", "player": b[4], "direction": "r2" }
+    }
+    else if(b[4]!="" && b[1]==b[4] && b[4]==b[7]){
+        rv = {"terminal": true, "type": "win", "player": b[4], "direction": "c2" }
+    }
+    else if(b[4]!="" && b[2]==b[4] && b[4]==b[6]){
+        rv = {"terminal": true, "type": "win", "player": b[4], "direction": "d1"  }
     }
     else if( b[6]!="" && b[6]==b[7] && b[7]==b[8]){
-        rv = {"terminal": true, "type": "win", "player": b[6] }
+        rv = {"terminal": true, "type": "win", "player": b[6], "direction": "r3" }
     }
     else if(b[2]!="" && b[2]==b[5] && b[5]==b[8]){
-        rv = {"terminal": true, "type": "win", "player": b[2] }
+        rv = {"terminal": true, "type": "win", "player": b[2], "direction": "c3" }
     }
     else if(c[0]+c[1]== 9 ){
         rv = {"terminal": true, "type": "draw", "player": "" }
@@ -135,7 +147,8 @@ function draw(){
     num_players = slider.value();
 
   background(0)
-  stroke(255)
+  stroke(255);
+  strokeWeight(1);
 
   line( 160, 80, 160, 320 )
   line( 240, 80, 240, 320 )
@@ -159,6 +172,48 @@ function draw(){
   text( "0", 450, 210 );
   text( "1", 480, 210 );
   text( "2", 510, 210 ); 
+
+  dir = terminal_state(b)[ "direction" ];
+  if( dir == "r1"){
+    stroke(255, 204, 0);
+    strokeWeight(4);
+    line( 95, 115, 300, 115);    
+  }
+  else if( dir == "r2"){
+    stroke(255, 204, 0);
+    strokeWeight(4);
+    line( 95, 200, 300, 200);    
+  }  
+  else if( dir == "r3"){
+    stroke(255, 204, 0);
+    strokeWeight(4);
+    line( 95, 285, 300, 285);   
+  }    
+  else if( dir == "c1"){
+    stroke(255, 204, 0);
+    strokeWeight(4);
+    line( 115, 100, 115, 305);     
+  }
+  else if(dir == "c2"){
+    stroke(255, 204, 0);
+    strokeWeight(4);
+    line( 198, 100, 198, 305);   
+  }
+  else if(dir == "c3"){
+    stroke(255, 204, 0);
+    strokeWeight(4);
+    line( 280, 100, 280, 305);         
+  }
+  else if( dir == "d1"){
+    stroke(255, 204, 0);
+    strokeWeight(4);
+    line( 95, 305, 300, 100);     
+  }
+  else if( dir == "d2"){
+    stroke(255, 204, 0);
+    strokeWeight(4);
+    line( 95, 100, 300, 305);     
+  }
 }
 
 function play_next_move(){
